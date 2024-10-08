58e7fc70c4c22c742d718f644147f9a282190ddd
hailey <dohye0806@gmail.com>
2024-10-08 14:16:27 +0900

    [FIX] task-field cursor -> text

--- src/components/TaskField.tsx
+++ src/components/TaskField.tsx
@@ -127,6 +127,7 @@ const TaskField = ({
             onClick={() => {
               setIsEditing(true);
             }}
+            style={{ cursor: 'text' }}
           >
             {' '}
             {currentContent}{' '}
